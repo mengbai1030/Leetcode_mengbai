e65ff4dcf4548f6484a6a22116ea2de33ba3a307
mengbai1030 <mengbai600@gmail.com>
2025-07-12 21:10:28 +0800

    Create Find Lucky Integer in an Array.cpp

---
+++ Find Lucky Integer in an Array.cpp
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/find-lucky-integer-in-an-array/description/?envType=daily-question&envId=2025-07-11
+
+/* Find Lucky Integer in an Array
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+ 
+
+Example 1:
+
+Input: arr = [2,2,3,4]
+Output: 2
+Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+Example 2:
+
+Input: arr = [1,2,2,3,3,3]
+Output: 3
+Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+Example 3:
+
+Input: arr = [2,2,2,3,3]
+Output: -1
+Explanation: There are no lucky numbers in the array.
+*/
+
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int ans = -1;
+        map<int, int> v;
+        for(int i=0; i<arr.size(); i++){
+            v[arr[i]]++;
+        }
+
+        for(auto it : v){
+            if(it.first == it.second){
+                ans = it.first;
+            }
+        }
+
+        return ans;
+    }
+};
